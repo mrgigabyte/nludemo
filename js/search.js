29d12c2aa30a9ffed895b0e7370812dabe3b8bbb
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2017-06-07 04:50:38 +0530

    Updated recent searches

--- js/search.js
+++ js/search.js
@@ -1,8 +1,21 @@
 document.getElementById("searchHeader").addEventListener("click", function() {
     $('.nav-cust').addClass("isActive");
+    $('.srch-container').addClass("isActive");
+    $('.search-container-parent').addClass('isActive');
+    showRecent();
 });
 
 
 document.getElementById("nav-cust").addEventListener("focusout", function() {
     $('.nav-cust').removeClass("isActive");
+    $('.srch-container').removeClass("isActive");
+    $('.search-container-parent').removeClass('isActive');
+    $('.search-container-parent').html("");
 });
+
+function showRecent(){
+    $('.search-container-parent').html("");
+    for(index in queries) {
+        $('.search-container-parent').append('<p class="search-suggestions">'+queries[index]+"</p>");
+    }
+}
