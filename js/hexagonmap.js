a66f4d886ffb303dcd07f50f12c6ac70d498c779
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2017-06-07 21:23:59 +0530

    changed the imagefile path

--- js/hexagonmap.js
+++ js/hexagonmap.js
@@ -5,6 +5,7 @@ function drawHexagonalMap(csvpath) {
     d3v3.select('.selected').html(treasury);
     d3v3.select('.notselected1').html(churn);
     d3v3.select('.notselected2').html(cross_sell);
+    
 	var center = [20.5937, 78.9629];
 	var osmUrl = '',
 		osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
