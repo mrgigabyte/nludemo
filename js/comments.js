bfb11ca0b0e5345c07c7bc00b5e5b0a9e6f06fef
abhishekgupta92 <abhishekgupta.iitd@gmail.com>
2017-06-08 10:27:00 +0530

    fixed css

--- js/comments.js
+++ js/comments.js
@@ -15,15 +15,6 @@ function addCommentButton(txt, funcname, btnId, xpos,ypos){
 	if (btnId) { var idTxt = `id="`+btnId+`"`}
 	var ele = `<button class="comment-btn" `+idTxt+` onclick="`+funcname+`()">`+txt+`</button>`;
     $('body').append(ele);
-    $('#'+btnId).css('position', 'fixed');
-    $('#'+btnId).css('top', ypos+'px'); //or wherever you want it
-    $('#'+btnId).css('left', xpos+'px');
-    $('#'+btnId).css('z-index', 1007);
-    $('#'+btnId).css('border-radius', "50%");
-    $('#'+btnId).css('border', "none");
-    $('#'+btnId).css('color', '#fff');
-    $('#'+btnId).css('background-image', "linear-gradient(207deg, #c50052, #920029)");
-    $('#'+btnId).css('padding', "16px 20px");
-    $('#'+btnId).css('cursor', "pointer");
-    $('#'+btnId).css('outline', "none");
+    $('#'+btnId).css('top', ypos - 24 +'px'); //or wherever you want it
+    $('#'+btnId).css('left', xpos - 24 +'px');
 }
