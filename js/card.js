7839f13410cc89cf4cf3af10aa1dba4be7045dbe
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2017-06-08 12:46:21 +0530

    fixed few things

--- js/card.js
+++ js/card.js
@@ -1,34 +1,34 @@
 var cross_sell=     `<div class="cross-sell">
                 How many of my customers can be contacted through email for an insurance product among those customers whom I have rejected for a loan? </div>
-                <img src="../images/img-crosssell-chart.png" srcset="../images/img-crosssell-chart@2x.png 2x,../images/img-crosssell-chart@3x.png 3x" class="img_crosssell_chart">`;
+                <img src="../images/img-crosssell-chart.png" srcset="../images/img-crosssell-chart@2x.png 2x,../images/img-crosssell-chart@3x.png 3x" class="img_crosssell_chart fix_margin_card">`;
 var up_sell=        `<div class="up-sell">
                 How many customers have a bank balance of more than 10 Cr and own collateralized property?
                 </div>
-                <img src="../images/img-upsell-chart.png" srcset="../images/img-upsell-chart@2x.png 2x, ../images/img-upsell-chart@3x.png 3x" class="img_upsell_chart">`;
+                <img src="../images/img-upsell-chart.png" srcset="../images/img-upsell-chart@2x.png 2x, ../images/img-upsell-chart@3x.png 3x" class="img_upsell_chart fix_margin_card">`;
 var credit_policy1=`<div class="credit-policy1">
                 If I were to increase the net industry profit margin on my BIL product by 10% what ratio of those rejected would we be able to offer a loan to?</div>
-                <img src="../images/img-credit-1-chart.png" srcset="../images/img-credit-1-chart@2x.png 2x,../images/img-credit-1-chart@3x.png 3x" class="img_credit_1_chart">`;
+                <img src="../images/img-credit-1-chart.png" srcset="../images/img-credit-1-chart@2x.png 2x,../images/img-credit-1-chart@3x.png 3x" class="img_credit_1_chart fix_margin_card">`;
 var credit_policy2=`<div class="credit-policy2">
                 If I were to switch to a definition of fraud that includes people who pay once but do not pay again, what would be the number of people whom I would reject on the basis of intent to pay?
                 </div>
-                <img src="../images/img-credit-2-chart.png" srcset="../images/img-credit-2-chart@2x.png 2x, ../images/img-credit-2-chart@3x.png 3x" class="img_credit_2_chart">`;
+                <img src="../images/img-credit-2-chart.png" srcset="../images/img-credit-2-chart@2x.png 2x, ../images/img-credit-2-chart@3x.png 3x" class="img_credit_2_chart fix_margin_card" style="margin:10px;">`;
 var kycauthentication=`<div class="kycauthentication">
                     How many people have registered biometric KYC?</div>
-                    <img src="../images/img-kyc-chart.png" srcset="../images/img-kyc-chart@2x.png 2x, ../images/img-kyc-chart@3x.png 3x" class="img_kyc_chart">`;
+                    <img src="../images/img-kyc-chart.png" srcset="../images/img-kyc-chart@2x.png 2x, ../images/img-kyc-chart@3x.png 3x" class="img_kyc_chart fix_margin_card">`;
 var salesefficiency=`<div class="salesefficiency">
                 What is the RoI of sales RMs by region for the digital business?
                 </div>
                 <img src="../images/img-sales-chart.png" srcset="../images/img-sales-chart@2x.png 2x, ../images/img-sales-chart@3x.png 3x"
-                class="img_sales_chart">`;
+                class="img_sales_chart fix_margin_card">`;
 var treasury=`<div class="treasury">
           What is the NIM by product for the last 5 years?
           </div>
         <img src="../images/img-treasury-chart.png" srcset="../images/img-treasury-chart@2x.png 2x,
-        ../images/img-treasury-chart@3x.png 3x" class="img_treasury_chart">`;
+        ../images/img-treasury-chart@3x.png 3x" class="img_treasury_chart fix_margin_card">`;
 var churn=`<div class="churn">
         What is the NIM by product for the last 5 years?
         </div>
-        <img src="../images/img-chum-chart.png" srcset="../images/img-chum-chart@2x.png 2x, ../images/img-chum-chart@3x.png 3x" class="img_chum_chart">`;
+        <img src="../images/img-chum-chart.png" srcset="../images/img-chum-chart@2x.png 2x, ../images/img-chum-chart@3x.png 3x" class="img_chum_chart fix_margin_card">`;
 var allproducts=`<div class="allproducts">
             What have been the product trends for ABFL in the last 12 months vs inflation
             </div>
@@ -38,3 +38,5 @@ var initialcard=`<div class="initialcard">
             Customer Growth for in Q4 in the last 4 years
             </div>`;
 var daily_digest='<div class="daily_digest">Your daily <span class="daily_digest_second">digest</span></div>';
+
+var cancel_save_type=`<div class="cancel_save_type">What have been the product trends for ABFL in the last 12 months vs inflation</div>`;
